5059c095b0effe2d18b5b4dda42fa723caf70828
Mikhail Libchenko <libchenkomikhail@gmail.com>
2021-12-31 06:42:43 +0300

    fix: GetCitizenByIdAsync changed to FindCitizenByIdAsync

--- Infrastructure/Interfaces/ICitizensService.cs
+++ Infrastructure/Interfaces/ICitizensService.cs
@@ -5,5 +5,5 @@ using Domain.Entities;
 public interface ICitizenService
 {
     public Task<List<Citizen>> GetCitizensAsync();
-    public Task<Citizen> GetCitizenByIdAsync(Guid citizenId);
+    public Task<Citizen?> FindCitizenByIdAsync(Guid citizenId);
 }
--- Infrastructure/Services/CitizensService.cs
+++ Infrastructure/Services/CitizensService.cs
@@ -2,7 +2,6 @@ namespace Infrastructure.Services;
 
 using DataAccess;
 using Domain.Entities;
-using Domain.Exceptions;
 using Interfaces;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,7 +15,6 @@ public class CitizensService : ICitizenService
     public async Task<List<Citizen>> GetCitizensAsync()
         => await _dbContext.Citizens.ToListAsync();
 
-    public async Task<Citizen> GetCitizenByIdAsync(Guid citizenId)
-        => await _dbContext.Citizens.SingleOrDefaultAsync(citizen => citizen.Id == citizenId)
-           ?? throw new Tele2Exception($"Citizen with Id {citizenId} doesn't exist");
+    public async Task<Citizen?> FindCitizenByIdAsync(Guid citizenId)
+        => await _dbContext.Citizens.SingleOrDefaultAsync(citizen => citizen.Id == citizenId);
 }
